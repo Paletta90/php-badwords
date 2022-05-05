3488d68af443d88e8683d58de4e6346b19b94597
DavideDiPietro <davide.dipietro3@gmail.com>
2022-05-05 10:29:23 +0200

    Utilizzo tag form con method GET

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
 <?php 
-$text='lorem ipsum dolor sit lorem, lorem adipiscing elit, sed do eiusmod tempor lorem ut labore et dolore magna aliqua. Impsum enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+$text='Lorem ipsum dolor sit lorem, lorem adipiscing elit, sed do eiusmod tempor lorem ut labore et dolore magna aliqua. Impsum enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
 // $arrayParole = $_GET; --> Posso iniserire più parola nell'Url 
 // Parola inserita tramite Uri da sostituire 
-$Badword = isset( $_GET['Badword'] ) ? $_GET['Badword'] : '';
+// $Badword = isset( $_GET['Badword'] ) ? $_GET['Badword'] : '';
 
 // Testo con parola sostituita
-$newText = str_ireplace($Badword, '***', $text);
+// $newText = str_ireplace($Badword, '***', $text);
+
+
+// Metodo Empty
+if( !empty( $_GET['Badword'] ) ){
+    $Badword = $_GET['Badword'];
+    $newText = str_ireplace($Badword, '***', $text);
+}else{
+    $Badword = '';
+    $newText = $text;
+}
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -21,6 +34,11 @@ $newText = str_ireplace($Badword, '***', $text);
 
 <body>
 
+    <form method="GET">
+        <input type="text" name="Badword">
+        <button>Invia</button>
+    </form>
+
     <!-- Stampo testo e lunghezza -->
     <h1>Testo iniziale: </h1>
     <p>- <?php echo $text ?> </p>
@@ -31,7 +49,6 @@ $newText = str_ireplace($Badword, '***', $text);
     <h2>Testo con parola sostituita: </h2>
     <p>- <?php echo $newText ?></p>
     <p>La lunghezza del nuovo testo è : <?php echo strlen($newText) ?></p>
-
 
 </body>
 
